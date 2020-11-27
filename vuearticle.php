f371c887b044a1fc5a3b0d8e9ab262a1f95676b6
vidalmat <72913872+vidalmat@users.noreply.github.com>
2020-11-27 13:13:06 +0100

    Add files via upload

---
+++ vuearticle.php
@@ -0,0 +1,45 @@
+<?php
+
+
+
+
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Vue Article</title>
+</head>
+    <body>
+
+
+        <article>
+
+        <div id="futura">
+
+            <h2>Futura, les dix technologies de 2020 qui vont changer le monde</h2>
+
+            <img src="images/futura.jpg" alt="">
+
+            <div class="bouton">
+                <p><a href="https://www.futura-sciences.com/sciences/actualites/magazine-science-10-technologies-2020-vont-changer-monde-6166/">Lire l'article</a></p>
+            </div>
+
+        </div>
+
+        <?= $html?>
+
+        <h3>Titre de mon article</h3>
+
+        <p class="paragraphe">Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi nostrum sit, suscipit adipisci, quibusdam explicabo earum saepe aperiam, ducimus necessitatibus facilis <br>
+            iusto consequatur soluta aspernatur error asperiores. Accusamus, laboriosam voluptatibus.</p>
+
+        </article>
+    
+    </body>
+</html>
